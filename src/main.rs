7a03d96ca4eec507c3dcf3e0246bab1157bbb7db
Rodrous <59244346+Rodrous@users.noreply.github.com>
2022-05-27 14:31:14 +0530

    FreeStanding Binary Complete

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-
+mod serial;
 mod vga_buffer;
 
 #[no_mangle]
@@ -17,6 +17,7 @@ pub extern "C" fn _start() -> ! {
 }
 
 /// This function is called on panic.
+#[cfg(not(test))] // new attribute
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
@@ -24,26 +25,24 @@ fn panic(info: &PanicInfo) -> ! {
 }
 
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
+fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
     exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
 fn trivial_assertion() {
-    print!("trivial assertion... ");
     assert_eq!(1, 1);
-    println!("[ok]");
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
 pub enum QemuExitCode {
     Success = 0x10,
-    Failed = 0x11,
+    Failure = 0x11,
 }
 
 pub fn exit_qemu(exit_code: QemuExitCode) {
@@ -51,5 +50,34 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
     unsafe {
         let mut port = Port::new(0xf4);
         port.write(exit_code as u32);
+    }
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failure);
+    loop {}
+}
+
+#[test_case]
+fn this_will_fail() {
+    assert_eq!(2, 1);
+}
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_println!("{} ...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
     }
 }
